5aa8f811d9d8420cf2f6755ed35086b0134f61a6
DjAbama <kurochkaseva08@gmail.com>
2026-07-01 21:00:37 +0300

    Add Create matrix function

--- main.c
+++ main.c
@@ -32,11 +32,49 @@ int Select_Operation() {
     return Operation_Variable;   
 }
 
+float** Create_Matrix(int rows, int colls){
+    float **matrix_pointer = calloc(rows, sizeof(float *));
 
+    if(matrix_pointer == NULL){
+        printf("Error: Not enough memory");
+        return NULL;
+    }
 
+    for(int current_ROW = 0; current_ROW < rows; current_ROW++){
+        matrix_pointer[current_ROW] = calloc(colls, sizeof(float));
+
+        if(matrix_pointer[current_ROW] == NULL){
+            printf("Error: Not enough memory");
+            return NULL;
+        }
+    }
+
+    for(int current_ROW = 0; current_ROW < rows; current_ROW++){
+
+        printf("Enter %d row numbers:", current_ROW);
+        for(int current_COLL = 0; current_COLL < colls; current_COLL++){
+            scanf("%f", &matrix_pointer[current_ROW][current_COLL]);
+        }
+        printf("\n");
+    }
+
+    return matrix_pointer;
+}
+
+void Delete_Matrix(float **matrix_ptr, int rows){
+    for(int current_ROW = 0; current_ROW < rows; current_ROW++){
+        free(matrix_ptr[current_ROW]);
+    }
+
+    free(matrix_ptr);
+}
 
 int main(){
     int Current_Operation = Select_Operation();
+
+    float **matrix1 = Create_Matrix(2, 3);
+
+    Delete_Matrix(matrix1, 2);
 
     return 0;
 }
